e4a4a2660ef9b15d795081a0ff16ddbb3d1e391f
creeperyang <yangcreeper@hotmail.com>
2016-04-08 23:29:10 +0800

    wont set value property of webpack context

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ module.exports = function(source) {
 	this.cacheable && this.cacheable();
     var callback = this.async();
     var value = source.toString();
-    // is css-loader output
     if (/module\.exports\s*=\s*require\s*\(/.test(value)) {
         value = this.exec(source, this.resource).toString();
     }
-    value = JSON.stringify(value);
-    this.value = [value];
-    callback(null, "module.exports = " + value + ";");
+    callback(null, 'module.exports = ' + JSON.stringify(value) + ';');
 };
